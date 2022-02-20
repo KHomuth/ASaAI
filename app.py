4de8a5c436c006641e2718bdeafd9d7c69253316
Kevin Homuth <kevin.homuth@haw-hamburg.de>
2022-02-20 14:45:54 +0100

    added visual information, accuracy in frontend, CSS changes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+## Code erstellt nach Tutorial, identsiche Codezeilen nach:
+## Parashar, A. (2020, 13. Oktober). Sentiment Analysis web app using NLTK and Heroku. medium. https://medium.com/pythoneers/sentiment-analysis-web-app-using-nltk-and-heroku-96ccd37c44ef
+
 from flask import Flask,render_template,request
 import pickle
 import re
@@ -19,10 +22,8 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method=='POST':
-        new_model = request.form['algorithm']
-        model = pickle.load(open('sentiment_analysis/' + new_model + '_review.pkl','rb'))
-        new_review = request.form['review']
-        new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+        review = request.form['review']
+        new_review = re.sub('[^a-zA-Z]', ' ', review)
         new_review = new_review.lower()
         new_review = new_review.split()
         ps = PorterStemmer()
@@ -32,8 +33,23 @@ def predict():
         new_review = ' '.join(new_review)
         new_corpus = [new_review]
         new_X_test = cv.transform(new_corpus).toarray()
-        pred = model.predict(new_X_test)
-        return render_template('result.html',prediction=pred)
+
+        if request.form['algorithm'] != 'all':
+            new_model = request.form['algorithm']
+            model = pickle.load(open('sentiment_analysis/' + new_model + '_review.pkl','rb'))
+            pred = model.predict(new_X_test)
+            return render_template('result.html',review=review,review_result=new_review,x_test=new_X_test,prediction=pred,name=new_model)
+        else:
+            model_nb = pickle.load(open('sentiment_analysis/nb_review.pkl','rb'))
+            model_lr = pickle.load(open('sentiment_analysis/lr_review.pkl','rb'))
+            model_dtc = pickle.load(open('sentiment_analysis/dtc_review.pkl','rb'))
+            model_rfc = pickle.load(open('sentiment_analysis/rfc_review.pkl','rb'))
+            pred_nb = model_nb.predict(new_X_test)
+            pred_lr = model_lr.predict(new_X_test)
+            pred_dtc = model_dtc.predict(new_X_test)
+            pred_rfc = model_rfc.predict(new_X_test)
+            return render_template('result_all.html',review=review,review_result=new_review,x_test=new_X_test,prediction_nb=pred_nb,prediction_lr=pred_lr,prediction_dtc=pred_dtc,prediction_rfc=pred_rfc)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
